15ffcc6111f217669081c2b41c9c072fa1b766fc
nazif-salisu <nazifsalisu2025@gmail.com>
2025-09-27 13:29:13 +0100

    Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Footer.css';
 import facebook from '../../Assets/facebookIcon.svg'
 import instagram from '../../Assets/instagramIcon.svg'
-import linkedin from '../../Assets/linkedinIcon.svg'
 import naza from '../../Assets/naza_logo1.png'
 
 const Footer = () => {
@@ -16,14 +15,11 @@ const Footer = () => {
     <div className="footer-links">
       <div className="footer-link">
         <div className="web-logos">
-          <a href="https://www.instagram.com/nazamarket">
+          <a href="https://www.facebook.com/nazifsalisu_official">
             <img src={facebook} alt="" className="web-logo"/>
           </a>
-          <a href="https://www.linkedin.com/naza">
+          <a href="https://www.instagram.com/nazifsalisu_official">
             <img src={instagram} alt="" className="web-logo"/>
-          </a>
-          <a href="https://www.pinteres.com/naza">
-            <img src={linkedin} alt="" className="web-logo" />
           </a>
         </div>
         <h1 className='h-footer'>Nazif Salisu</h1>
